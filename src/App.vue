f46feea60a4e64362e77c5b90000a148e7543acc
xyh <yaohua.xu@xiaotun.com>
2022-08-29 18:19:49 +0800

    初始化git仓库

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { zhCN, dateZhCN, darkTheme } from 'naive-ui';
+</script>
+
+<template>
+  <n-config-provider :theme="darkTheme" :locale="zhCN" :date-locale="dateZhCN">
+    <n-space vertical>
+    </n-space>
+  </n-config-provider>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+
+export default defineConfig({
+  plugins: [vue(),
+  // 自动引入插件
+  AutoImport({
+    imports: [
+      "vue",
+      {
+        "naive-ui": []
+      }
+    ]
+  }),
+  Components({
+    resolvers: [NaiveUiResolver()]
+  })],
+  base: "./",
+})
